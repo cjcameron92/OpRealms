0830f385ed189823d83e2fee9c8f7d62f8200f11
CarvalCam <cjcameron92@gmail.com>
2024-08-08 14:13:23 -0400

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
-import Image from 'next/image'
-import logo from "@/../public/logo.png"
+import Image from 'next/image';
+import logo from "@/../public/logo.png";
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-opstone flex flex-col items-center justify-center">
@@ -9,21 +10,21 @@ export default function Home() {
         <h2 className="text-2xl font-semibold mb-4">How to Join</h2>
         <div className="bg-white p-4 rounded-lg shadow-md">
           <h3 className="text-xl font-bold text-opred">Java Edition</h3>
-          <p className="mt-2">1. Open Minecraft and click on 'Multiplayer'</p>
-          <p>2. Click on 'Add Server'</p>
+          <p className="mt-2">1. Open Minecraft and click on &apos;Multiplayer&apos;</p>
+          <p>2. Click on &apos;Add Server&apos;</p>
           <p>3. Enter the server address: <span className="font-bold">java.oprealms.com</span></p>
-          <p>4. Click 'Done' and join the server</p>
+          <p>4. Click &apos;Done&apos; and join the server</p>
         </div>
         <div className="bg-white p-4 rounded-lg shadow-md mt-6">
           <h3 className="text-xl font-bold text-opred">Bedrock Edition</h3>
-          <p className="mt-2">1. Open Minecraft and click on 'Play'</p>
-          <p>2. Click on 'Servers' tab</p>
-          <p>3. Click on 'Add Server'</p>
+          <p className="mt-2">1. Open Minecraft and click on &apos;Play&apos;</p>
+          <p>2. Click on &apos;Servers&apos; tab</p>
+          <p>3. Click on &apos;Add Server&apos;</p>
           <p>4. Enter the server address: <span className="font-bold">bedrock.oprealms.com</span></p>
           <p>5. Enter the port: <span className="font-bold">19132</span></p>
-          <p>6. Click 'Save' and join the server</p>
+          <p>6. Click &apos;Save&apos; and join the server</p>
         </div>
       </div>
     </div>
-  )
+  );
 }
